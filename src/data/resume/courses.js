e336acb6a5145b3a786b7e1e9d6b1b8bc09f1d80
gizhou <czo2@illinois.edu>
2021-09-02 11:47:52 -0500

    completed transformation

--- src/data/resume/courses.js
+++ src/data/resume/courses.js
@@ -2,50 +2,50 @@ const courses = [
   {
     title: 'Cognitive Psychology',
     number: 'PSYC 224',
-    link: 'http://stanford.edu/class/ee364a/',
+    link: '',
     university: 'UIUC',
   },
   {
     title: 'Intro to Computer Science',
     number: 'CS 125',
-    link: 'http://cs229.stanford.edu/',
+    link: '',
     university: 'UIUC',
   },
   {
     title: 'Discrete Structure',
     number: 'CS 173',
-    link: 'http://cs231n.stanford.edu/',
+    link: '',
     university: 'UIUC',
   },
   {
     title: 'Software Design Studio',
     number: 'CS 126',
     link:
-      'http://scpd.stanford.edu/search/publicCourseSearchDetails.do;jsessionid=561188A06434D7D97953C4706DE12831?method=load&courseId=11685',
+      '',
     university: 'UIUC',
   },
   {
     title: 'Data Structure',
     number: 'CS 225',
-    link: 'http://web.stanford.edu/class/cme304/',
+    link: '',
     university: 'UIUC',
   },
   {
     title: 'Computer Architecture',
     number: 'CS 233',
-    link: 'http://stanford.edu/~rezab/discrete/',
+    link: '',
     university: 'UIUC',
   },
   {
     title: 'Game Theory',
     number: 'ECON 437',
-    link: 'http://web.stanford.edu/class/math220/index.html',
+    link: '',
     university: 'UIUC',
   },
   {
     title: 'Economic Statistics II',
     number: 'ECON 203',
-    link: 'http://ee263.stanford.edu/',
+    link: '',
     university: 'UIUC',
   },
 ];
